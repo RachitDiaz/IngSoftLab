48c6d2fa769fe12c7d4d8428bced6bcb7d218797
RachitDiaz <rachit.diaz08@gmail.com>
2025-03-15 11:55:41 -0600

    creación del proyecto y solución del laboratorio 1

---
+++ Laboratorio1_c12569/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_c12569.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
