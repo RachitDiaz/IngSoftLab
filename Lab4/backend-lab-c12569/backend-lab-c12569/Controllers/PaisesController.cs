b3cdb9b006fc957da249861f5d0afa8b65763d6d
RachitDiaz <rachit.diaz08@gmail.com>
2025-04-13 19:13:36 -0600

    Entrega del Lab 4 con el API funcional

---
+++ Lab4/backend-lab-c12569/backend-lab-c12569/Controllers/PaisesController.cs
@@ -0,0 +1,25 @@
+﻿using backend_lab.Handlers;
+using backend_lab_c12569.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab_c12569.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        private readonly PaisesHandler _paisesHandler;
+        public PaisesController()
+        {
+            _paisesHandler = new PaisesHandler();
+        }
+        [HttpGet]
+        public List<PaisModel> Get()
+        {
+            var paises = _paisesHandler.ObtenerPaises();
+            return paises;
+        }
+
+    }
+}
